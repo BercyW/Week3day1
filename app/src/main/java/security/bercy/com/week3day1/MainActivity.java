5e7151d27d47a8d582182e22ab0f7676eaf9b0dc
Bercy <adnmks@gmail.com>
2017-12-13 13:04:09 -0500

    1

--- app/src/main/java/security/bercy/com/week3day1/MainActivity.java
+++ app/src/main/java/security/bercy/com/week3day1/MainActivity.java
@@ -1,6 +1,7 @@
 package security.bercy.com.week3day1;
 
 import android.content.Context;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -16,62 +17,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button btn_tom;
     private Button btn_clooney;
     private Button btn_will;
-    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_tom = findViewById(R.id.tomcruise);
-        btn_clooney = findViewById(R.id.clooney);
-        btn_will = findViewById(R.id.willsmith);
+        btn_tom = findViewById(R.id.btn_tom);
+        btn_clooney = findViewById(R.id.btn_clooney);
+        btn_will = findViewById(R.id.btn_will);
 
-
-
-        imageView = findViewById(R.id.imgFragment);
         btn_tom.setOnClickListener(this);
         btn_clooney.setOnClickListener(this);
         btn_will.setOnClickListener(this);
-        replaceFragment(new RightFragment(),new ImageFragment());
 
-    }
-
-
-
-    public void replaceFragment(RightFragment detail, ImageFragment img) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction transaction1 = fragmentManager.beginTransaction();
-        transaction1.replace(R.id.rightFragmentTop,detail).commit();
-        FragmentTransaction transaction2 = fragmentManager.beginTransaction();
-        transaction2.replace(R.id.rightFragmentBottom,img).commit();
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("", "onResume: ");
-
-        TextView tvDetail = findViewById(R.id.tvFragment);
-        tvDetail.setText("Thomas Cruise Mapother IV, known professionally as Tom Cruise, is an American actor and producer.");
     }
 
     @Override
     public void onClick(View view) {
-        switch(view.getId()) {
-            case R.id.tomcruise:
+        switch (view.getId()) {
 
-                replaceFragment(new RightFragment(),new ImageFragment());
+            case R.id.btn_tom:
+                replaceFragment(new TomCruseFragment());
+
 
                 break;
-            case R.id.clooney:
+            case R.id.btn_clooney:
+                replaceFragment(new ClooneyFragment());
                 break;
-            case R.id.willsmith:
-                break;
+            case R.id.btn_will:
+                replaceFragment(new WillSmithFragment());
 
+
+                break;
 
 
         }
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction1 = fragmentManager.beginTransaction();
+        transaction1.replace(R.id.rightFragment,fragment).commit();
+
+
     }
 }
